bb657d7931cd808000479a16d3a1a2ee74a71414
MurgrayDJ <murgraydj@gmail.com>
2023-03-31 15:56:51 -0400

    Creates Node class with necessary attributes
    The attributes are data, left node, and right node.

--- node.rb
+++ node.rb
@@ -1,3 +1,15 @@
 #Author: Murgray D. John
 #Date: 3/31/2023
 #Pupose: To create node objects for a balanced Binary Search Tree
+
+class Node
+  attr_reader @data 
+  attr_accessor @left
+  attr_accessor @right
+
+  def initialize(data)
+    @data = data 
+    @left = null
+    @right = null
+  end
+end

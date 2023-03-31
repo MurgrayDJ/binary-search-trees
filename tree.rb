09f519602eb1919d9a57d9c846989f6bbac26bf3
MurgrayDJ <murgraydj@gmail.com>
2023-03-31 16:10:53 -0400

    Adds Tree class with necessary attributes
    Adds root attribute. On initialization class takes in an array. The root
    is initialized using the #build_tree method (not implemented) by passing
    the array to method.

--- tree.rb
+++ tree.rb
@@ -1,3 +1,11 @@
 #Author: Murgray D. John
 #Date: 3/31/2023
 #Purpose: To create balanced binary search trees
+
+class Tree
+  attr_accessor :root
+
+  def initialize(sorted_array)
+    @root = build_tree(sorted_array)
+  end
+end

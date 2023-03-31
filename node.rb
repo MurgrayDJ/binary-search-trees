59eaf255b943cd74ebacdd1f55bf35a7015aaaee
MurgrayDJ <murgraydj@gmail.com>
2023-03-31 16:04:49 -0400

    Includes and implements Comparable for Node class
    The Nodes are compared based on their data. This commit also corrects
    some bugs.

--- node.rb
+++ node.rb
@@ -3,13 +3,27 @@
 #Pupose: To create node objects for a balanced Binary Search Tree
 
 class Node
-  attr_reader @data 
-  attr_accessor @left
-  attr_accessor @right
+  attr_reader :data 
+  attr_accessor :left
+  attr_accessor :right
+  include Comparable
 
   def initialize(data)
     @data = data 
-    @left = null
-    @right = null
+    @left = nil
+    @right = nil
+  end
+
+  def <=>(other_node)
+    @data <=> other_node.data
   end
 end
+
+node1 = Node.new(1)
+node2 = Node.new(2)
+
+puts node1 <=> node2
+
+node3 = Node.new(2)
+
+puts node2 <=> node3

e28161a952f83a73697425a4c84eb3053eed2853
MurgrayDJ <murgraydj@gmail.com>
2023-04-01 22:08:28 -0400

    Adds Odin Project's suggested pretty print method
    This method was written by another student.

--- tree.rb
+++ tree.rb
@@ -8,4 +8,10 @@ class Tree
   def initialize(sorted_array)
     @root = build_tree(sorted_array)
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
